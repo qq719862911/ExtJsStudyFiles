665920bd49b5414811f681abca59b10a29aa6d44
guohaiyi <516877626@qq.com>
2018-03-21 16:58:05 +0800

    exit

---
+++ extjs3/index2.js
@@ -0,0 +1,15 @@
+
+         Ext.onReady(function() {
+         Ext.create('Ext.Panel', {
+            renderTo: 'helloWorldPanel',
+            height: 100,
+            width: 200,
+            title: 'Hello world',
+            html: 'First Ext JS Hello World Program'
+            });
+            Ext.create('Ext.Panel',{
+            renderTo:'panel2',
+            title:'haiyi',
+            html:'Please say ok!'
+            });
+         });
